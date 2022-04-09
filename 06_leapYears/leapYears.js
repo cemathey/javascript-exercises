a6ade3e6b8545b7875af89c2d5ab63cf2e4cc6fc
Christopher Mathey <emathey@protonmail.com>
2022-04-09 14:07:17 +0400

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  divisibleBy4 = year % 4 === 0;
+  divisibleBy100 = year % 100 === 0;
+  divisibleBy400 = year % 400 === 0;
 
+  if (divisibleBy4 && divisibleBy100 && divisibleBy400) {
+    return true;
+  } else if (divisibleBy4 && !divisibleBy100) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
