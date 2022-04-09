6961c940a9f6324b4a1fc20b6fbe21a8d61bfad5
Christopher Mathey <emathey@protonmail.com>
2022-04-09 13:16:24 +0400

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  reversed = [];
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+
+  return reversed.join("");
 };
 
 // Do not edit below this line
