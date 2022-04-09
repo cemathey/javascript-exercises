1c5559c20019dc959dabca9460de2eb5722492d7
Christopher Mathey <emathey@protonmail.com>
2022-04-09 14:15:37 +0400

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (degrees) {
+  let celsiusDegrees = (degrees - 32) * (5.0 / 9.0);
+  return Math.round(celsiusDegrees * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (degrees) {
+  let fahrenheitDegrees = degrees * (9.0 / 5.0) + 32;
+  return Math.round(fahrenheitDegrees * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
