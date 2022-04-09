e194e6291236a872f78b7882b77c4e4ff54dbb9b
Christopher Mathey <emathey@protonmail.com>
2022-04-09 13:13:46 +0400

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
+  if (count < 0) {
+    return "ERROR";
+  }
 
+  strings = [];
+
+  for (let i = 0; i < count; i++) {
+    strings.push(str);
+  }
+
+  return strings.join("");
 };
 
 // Do not edit below this line
