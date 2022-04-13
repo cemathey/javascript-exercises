0bd77d91d22880edc33848c6147df151fe8d188c
Christopher Mathey <emathey@protonmail.com>
2022-04-13 11:08:43 +0400

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((prev, curr) => {
+    let prevAge =
+      (prev.yearOfDeath ?? new Date().getFullYear()) - prev.yearOfBirth;
+    let currAge =
+      (curr.yearOfDeath ?? new Date().getFullYear()) - curr.yearOfBirth;
 
+    return prevAge > currAge ? prev : curr;
+  });
 };
 
 // Do not edit below this line
