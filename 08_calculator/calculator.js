307c6b62dc148a2ee56d0d24c3ff341bf26b3b77
Christopher Mathey <emathey@protonmail.com>
2022-04-13 09:21:54 +0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (m, n) {
+  return m + n;
 };
 
-const subtract = function() {
-	
+const subtract = function (m, n) {
+  return m - n;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((acc, n) => acc + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((acc, n) => acc * n, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  num = parseInt(num);
+  if (Number.isNaN(num)) {
+    throw new TypeError(`${num} is not a number.`);
+  }
+
+  if (num < 0) {
+    throw new TypeError("Factorials of negative numbers are undefined.");
+  }
+
+  // default value is the factorial of 0
+  let acc = 1;
+  while (num > 1) {
+    acc *= num;
+    num--;
+  }
+
+  return acc;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
