b9f9fd39567acf17d946c87b3ed6a412d9d1883b
Christopher Mathey <emathey@protonmail.com>
2022-04-13 10:37:53 +0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (nth) {
+  // Recursive fibonacci without memoizing is evil!
+  // Iteratively calculate/store fibonacci numbers
+  let fibs = [1, 1];
 
+  nth = parseInt(nth);
+
+  if (!nth || nth < 0) return "OOPS";
+
+  switch (nth) {
+    // Use the classical definition where fib(0) = 0
+    case 0:
+      return 0;
+    case 1:
+    case 2:
+      return 1;
+  }
+
+  // After validating and checking for the 1st or 2nd fib
+  // Start on the 3rd fib and calculate up to it
+  for (let i = 2; i < nth; i++) {
+    let newFib = fibs[i - 1] + fibs[i - 2];
+    fibs.push(newFib);
+  }
+
+  return fibs.slice(-1)[0];
 };
 
 // Do not edit below this line
